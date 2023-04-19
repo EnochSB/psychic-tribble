20ae6e953058ac43692b0fa0446a45d22dec7b38
SungBum Ha <gktjd425@naver.com>
2023-04-19 15:50:57 +0900

    allowed_hosts

--- crud/settings.py
+++ crud/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-psychic-tribble-17xqnr2algnav219.sel3.cloudtype.app']
 
 
 # Application definition
